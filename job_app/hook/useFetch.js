acc2e1b63242992e27e55300d897c6246ee4e16b
DevFate <umerqamar0333@gmail.com>
2024-04-01 16:27:08 -0400

    Base App with css designs and main functions

---
+++ job_app/hook/useFetch.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+}
